a989e6fe61861044153704493dba895c688cd23d
Beckudrup <54031477+Beckudrup@users.noreply.github.com>
2024-11-18 18:13:54 +0100

    Updatering på min legeplads

--- Assets/Scripts/Netcode Scripts/VRrigReferences.cs
+++ Assets/Scripts/Netcode Scripts/VRrigReferences.cs
@@ -27,17 +27,6 @@ public class VRrigReferences : MonoBehaviour
     }
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void setNetworkPlayer(networkPlayer myPlayer)
     {
