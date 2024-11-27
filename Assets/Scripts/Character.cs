3052850fbe9040f525afd8ddf6e2e7455fc4b7f6
deadlap <llabildgaard@hotmail.com>
2024-11-27 16:30:13 +0100

    endnu mere fix

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -27,11 +27,14 @@ public class Character : MonoBehaviour {
             OwnXROrigin = GameObject.FindWithTag("Player");
             if (GetComponent<NetworkObject>().NetworkObjectId == 1){
                 SetGameLayerRecursive(gameObject, LayerMask.NameToLayer("Player1"));
+                SetGameLayerRecursive(OwnXROrigin, LayerMask.NameToLayer("Player1"));
             } if (GetComponent<NetworkObject>().NetworkObjectId == 2){
                 SetGameLayerRecursive(gameObject, LayerMask.NameToLayer("Player2"));
+                SetGameLayerRecursive(OwnXROrigin, LayerMask.NameToLayer("Player2"));
             }
         }
         PlayerRigidbody = OwnXROrigin.GetComponent<Rigidbody>();
+        
         KnockBackVector = Vector3.zero;
     }
 
