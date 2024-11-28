5410e1086f2b9e4063fb376da240029fa1daa37d
deadlap <llabildgaard@hotmail.com>
2024-11-28 13:48:56 +0100

    hej

--- Assets/Scripts/HandFollow.cs
+++ Assets/Scripts/HandFollow.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class HandFollow : MonoBehaviour {
     // Start is called before the first frame update
     [SerializeField] GameObject HandToFollow;
-     
+    
     void Start() {
         
     }
