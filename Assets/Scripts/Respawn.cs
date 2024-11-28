a8f683153e1ce22880feaf1c8772725ce9be8e24
deadlap <llabildgaard@hotmail.com>
2024-11-28 22:02:38 +0100

    Update Respawn.cs

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -7,21 +7,25 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Respawn : MonoBehaviour
 {
     public Transform xrRig;
-    public Transform teleportTarget;
-    public Transform player;
+    public Transform teleportTarget1;
+    public Transform teleportTarget2;
+    // public Transform player;
     public List<GameObject> gameObjects;
-    private void OnCollisionEnter(Collision collision)
-    {
+    private void OnCollisionEnter(Collision collision) {
         Teleport();
         SetActive();
     }
 
-     private void Teleport()
-    {
-        xrRig.position = teleportTarget.position;
-        xrRig.rotation = teleportTarget.rotation;
-        player.position = xrRig.position;
-        player.rotation = xrRig.rotation;
+     private void Teleport() {
+        if (xrRig.tag == "Player1") {
+            xrRig.position = teleportTarget1.position;
+            xrRig.rotation = teleportTarget1.rotation;
+        } else {
+            xrRig.position = teleportTarget2.position;
+            xrRig.rotation = teleportTarget2.rotation;
+        }
+        // player.position = xrRig.position;
+        // player.rotation = xrRig.rotation;
 
     }
 
